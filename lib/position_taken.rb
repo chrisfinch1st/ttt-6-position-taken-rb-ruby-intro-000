bb2af370d7f8e13ff77e7fe8ebf3e2ae5037c239
Christopher Finch <christopher.h.finch@gmail.com>
2018-01-26 07:05:52 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,9 @@
 # code your #position_taken? method here!
 def position_taken?(board,index)
-!(board[index]==" "||"")
+if (board[index]==" "||"")
+false
 #if board[index]=="X"||"O"
 #  true
 
-#end
+end
 end
