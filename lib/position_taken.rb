b003d1d49a9be45b294a5b2d430146a3e2f52039
Christopher Finch <christopher.h.finch@gmail.com>
2018-01-26 06:36:00 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board,index)
 if board[index]=="X"||"O"
-
-elsif !(board[index]==" "||"")
+  true
 end
+#elsif !(board[index]==" "||"")
+#end
 end
